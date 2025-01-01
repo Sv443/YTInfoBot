9ba3b8ce582b966564ac211a3a2f9b8e3a014468
Sv443 <contact@sv443.net>
2025-01-01 03:44:03 +0100

    ref: remove ready event & join fs.ts into misc.ts

--- src/events/_events.ts
+++ src/events/_events.ts
@@ -1,11 +1,9 @@
 import { GuildCreateEvt } from "@evt/GuildCreate.ts";
 import { GuildDeleteEvt } from "@evt/GuildDelete.ts";
 import { MessageCreateEvt } from "@evt/MessageCreate.ts";
-import { ReadyEvt } from "@evt/Ready.ts";
 
 export const events = [
   GuildCreateEvt,
   GuildDeleteEvt,
   MessageCreateEvt,
-  ReadyEvt,
 ] as const;
