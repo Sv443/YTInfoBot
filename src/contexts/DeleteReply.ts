400457dd763845cc240d8c0a2f6e585e015d9d26
Sv443 <contact@sv443.net>
2025-01-29 15:11:18 +0100

    feat: DeleteReply ctx

--- src/contexts/DeleteReply.ts
+++ src/contexts/DeleteReply.ts
@@ -1,9 +1,7 @@
 import { ContextCommand } from "@lib/Command.js";
-import { useEmbedify } from "@lib/embedify.js";
+import { Col, useEmbedify } from "@lib/embedify.js";
 import { getLocMap, tr } from "@lib/translate.js";
 import { ApplicationCommandType, ContextMenuCommandBuilder, InteractionContextType, PermissionFlagsBits, type ContextMenuCommandInteraction } from "discord.js";
-
-//TODO:
 
 export class DeleteReplyCtx extends ContextCommand {
   constructor() {
@@ -19,9 +17,35 @@ export class DeleteReplyCtx extends ContextCommand {
   //#region pb:run
 
   public async run(int: ContextMenuCommandInteraction) {
-    return int.reply({
-      ...useEmbedify("CONTEXT_TEMPLATE"),
-      ephemeral: true,
-    });
+    await int.deferReply({ ephemeral: true });
+    const t = await DeleteReplyCtx.getTrFunc(int);
+
+    try {
+
+      if(!int.isMessageContextMenuCommand())
+        return int.editReply(useEmbedify(t("errors.unknown"), Col.Error));
+
+      const { targetMessage: { reference } } = int;
+
+      if(!reference)
+        return int.editReply(useEmbedify(t("errors.messageInaccessible"), Col.Error));
+
+      const refMsg = (await int.channel?.messages.fetch({ around: reference.messageId, limit: 1 }))?.at(0);
+
+      if(refMsg?.author.id === int.user.id) {
+        try {
+          await int.targetMessage.delete();
+          return int.editReply(useEmbedify(t("commands.delete_reply_ctx.successMessage"), Col.Success));
+        }
+        catch {
+          return await int.editReply(useEmbedify(t("errors.messageInaccessible"), Col.Error));
+        }
+      }
+      else
+        return await int.editReply(useEmbedify(t("errors.notAuthor"), Col.Error));
+    }
+    catch {
+      return await int.editReply(useEmbedify(t("errors.unknown"), Col.Error));
+    }
   }
 }
